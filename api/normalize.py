7cb5ab414a6db2c9796efc2454d7b7ae0d71ed3e
Vishalvasanji <49619311+Vishalvasanji@users.noreply.github.com>
2025-10-09 14:21:11 -0500

    Update normalize.py

--- api/normalize.py
+++ api/normalize.py
@@ -280,9 +280,3 @@ async def normalize(
     }
     
     return StreamingResponse(generate_multipart(), headers=headers)
-
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.getenv("PORT", "8080"))
-    uvicorn.run(app, host="0.0.0.0", port=port)
